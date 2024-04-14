2447ac1e8302f8dda24cb71458d1e4544e1c2028
bujhm <Bujhm99@gmail.com>
2024-04-14 11:39:38 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,41 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-from taxi.models import Driver, Car, Manufacturer
+from taxi.models import Manufacturer, Driver, Car
 
 
-def index(request):
-    """View function for the home page of the site."""
+def index(request: HttpRequest) -> HttpResponse:
+    num_cars = Car.objects.count()
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    context = {"num_cars": num_cars,
+               "num_drivers": num_drivers,
+               "num_manufacturers": num_manufacturers}
+    return render(request, template_name="taxi/index.html", context=context)
 
-    context = {
-        "num_drivers": Driver.objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
-    }
 
-    return render(request, "taxi/index.html", context=context)
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__drivers")
